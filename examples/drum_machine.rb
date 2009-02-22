cb986431a39c6e5f08fb9ec86e8338b54a78ef3a
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2009-02-21 17:57:14 -0800

    Playing with the drum machine.

--- examples/drum_machine.rb
+++ examples/drum_machine.rb
@@ -11,30 +11,37 @@ class DrumMachine
       MIDIProgramChangeMessage.new(:channel => 10, :program => 26))
     @track.add_midi_channel_message(0.0,
       MIDIControlChangeMessage.new(:channel => 10, :number => 32, :value => 1))
-
     @player.sequence = @sequence
     build_track
   end
   
-  def play(beat, note, velocity=80, channel=10)
+  def drum(beat, note)
     @track.add_midi_note_message(beat,
       MIDINoteMessage.new(:note     => note,
-                          :velocity => velocity,
-                          :channel  => channel,
+                          :velocity => 80,
+                          :channel  => 10,
                           :duration => 0.1))
   end
   
-  KICK1 = 32
-  KICK2 = 36
-  SNARE = 40
+  def kick1(beat)
+    drum(beat, 32)
+  end
+
+  def kick2(beat)
+    drum(beat, 36)
+  end
+  
+  def snare(beat)
+    drum(beat, 40)
+  end
   
   def build_track
-    (0..15).each do |beat|
-      play(beat, KICK1) # every downbeat
-      play(beat+0.5, KICK2) # every upbeat
+    0.upto(15) do |beat|
+      kick1(beat)     # every downbeat
+      kick2(beat+0.5) # every upbeat
       if beat % 4 == 0
         extra = (beat % 8)/4
-        play(beat+extra, SNARE)
+        snare(beat+extra)
       end
     end
   end
