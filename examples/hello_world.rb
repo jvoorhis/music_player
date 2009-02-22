dde722c7c7bfa07ccd4f7536901919f631193498
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2009-02-22 14:46:21 -0800

    Slightly revamped examples/test.rb – now examples/hello_world.rb.

---
+++ examples/hello_world.rb
@@ -0,0 +1,23 @@
+require 'audio_toolbox'
+include AudioToolbox
+
+player = MusicPlayer.new
+sequence = MusicSequence.new
+player.sequence = sequence
+sequence.midi_endpoint = CoreMIDI.get_destination(ARGV.shift.to_i)
+
+tempo = sequence.tracks.tempo
+tempo.add 0.0, ExtendedTempoEvent.new(:bpm => 120.0)
+
+track = MusicTrack.new(sequence)
+track.add 0.0, MIDINoteMessage.new(:note => 60, :velocity => 64)
+track.add 1.0, MIDINoteMessage.new(:note => 64, :velocity => 96)
+track.add 2.0, MIDINoteMessage.new(:note => 67, :velocity => 110)
+track.add 3.0, MIDINoteMessage.new(:note => 60, :velocity => 110, :duration => 2.0)
+track.add 3.0, MIDINoteMessage.new(:note => 64, :velocity => 110, :duration => 2.0)
+track.add 3.0, MIDINoteMessage.new(:note => 67, :velocity => 110, :duration => 2.0)
+track.add 3.0, MIDINoteMessage.new(:note => 72, :velocity => 110, :duration => 2.0)
+
+player.start
+sleep 3
+player.stop
