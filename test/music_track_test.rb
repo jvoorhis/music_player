d78c776e5f937eada255a43d6abfc5b09fbf3845
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2009-03-02 19:38:51 -0800

    MusicTrack#initialize can also set track properties.

--- test/music_track_test.rb
+++ test/music_track_test.rb
@@ -5,11 +5,11 @@ class MusicTrackTest < Test::Unit::TestCase
     @sequence = MusicSequence.new
     @track = @sequence.tracks.new
   end
-
+  
   def test_iterator
     assert_kind_of MusicEventIterator, @track.iterator
   end
-
+  
   def test_enumerable
     @track.add 0, ev1=MIDINoteMessage.new(:note => 60)
     @track.add 1, ev2=MIDINoteMessage.new(:note => 64)
@@ -17,19 +17,19 @@ class MusicTrackTest < Test::Unit::TestCase
     
     assert_equal [ev1, ev2, ev3], @track.map { |x| x }
   end
-
+  
   def test_loop_info
     assert_equal({ :duration => 0.0, :number => 1 }, @track.loop_info)
     @track.loop_info = { :duration => 100.0, :number => 42 }
     assert_equal({ :duration => 100.0, :number => 42 }, @track.loop_info)
   end
-
+  
   def test_offset
     assert_equal 0.0, @track.offset
     @track.offset = 1.0
     assert_equal 1.0, @track.offset
   end
-
+  
   def test_mute
     assert_equal false, @track.mute
     @track.mute = true
@@ -41,7 +41,7 @@ class MusicTrackTest < Test::Unit::TestCase
     @track.solo = true
     assert_equal true, @track.solo
   end
-
+  
   def test_length
     assert_equal 0, @track.length
     
@@ -64,5 +64,17 @@ class MusicTrackTest < Test::Unit::TestCase
     # 480 is the default resolution. 960 is also common.
     assert_equal 480, @sequence.tracks.tempo.resolution
     assert_raise(ArgumentError) { @track.resolution }
+  end
+
+  def test_initialize
+    track = @sequence.tracks.new(:loop_info => { :duration => 10,
+                                                 :number   => 3},
+                                 :mute      => true,
+                                 :solo      => true,
+                                 :length    => 10)
+    assert_equal({:duration => 10, :number => 3}, track.loop_info)
+    assert track.mute
+    assert track.solo
+    assert_equal 10, track.length
   end
 end
